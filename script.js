e1ad136a7aa7ed1fba3345bb319f6623d52b3197
WillyHB <william.hansen.baird@icloud.com>
2021-03-05 21:36:12 +0100

    Added Basic article

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+var div = document.createElement("div");
 
+div.className = "NewsElements";
+
+div.appendChild(document.createElement("div"));
+
+var article = document.createElement("article");
+var image = document.createElement("img");
+image.src = "Images/OIP.jfif"
+
+article.appendChild(image);
+div.appendChild(article);
+
+document.head.appendChild(div);
