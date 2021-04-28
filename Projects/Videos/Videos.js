8723ff1e389b6687f86c49d232235c228959b82a
WillyHB <william.hansen.baird@icloud.com>
2021-04-28 22:01:42 +0200

    Added Video Tabs

---
+++ Projects/Videos/Videos.js
@@ -0,0 +1,17 @@
+var active = true;
+function Click(channel) {
+    if (active) {
+        if (channel.id != "WillyHB") {
+            channel.setAttribute('style', 'background-color: white; color: orange');
+            document.getElementById("WillyHB").setAttribute('style', 'background-color: rgb(246, 246, 246); color: white');
+            active = false;
+        }
+    }
+    else {
+        if (channel.id == "WillyHB") {
+            channel.setAttribute('style', 'background-color: white; color: orange');
+            document.getElementById("AwfulWillyHBSofa").setAttribute('style', 'background-color: rgb(246, 246, 246); color: white');
+            active = true;
+        }
+    }
+}
