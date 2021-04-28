26deb348d726e2b13f85d91f7b023bb8b30abcd8
WillyHB <william.hansen.baird@icloud.com>
2021-04-29 00:11:36 +0200

    Got basic video template loading working

--- Projects/Videos/Videos.js
+++ Projects/Videos/Videos.js
@@ -12,6 +12,27 @@ function Click(channel) {
             channel.setAttribute('style', 'background-color: white; color: orange');
             document.getElementById("AwfulWillyHBSofa").setAttribute('style', 'background-color: rgb(246, 246, 246); color: white');
             active = true;
+            var Video = document.createElement("div");
+            Video.className = "Video";
+            var Vid = document.createElement("iframe");
+            Vid.className = "Vid";
+            var InfoHeader = document.createElement("div");
+            InfoHeader.className = "InfoHeader";
+            var Title = document.createElement("h2");
+            Title.className = "Title";
+            Title.textContent = "Test";
+            var Date = document.createElement("h2");
+            Date.className = "Date";
+            Date.textContent = "April 5, 2020";
+            var Description = document.createElement("h2");
+            Description.className = "Description";
+            Description.textContent = "This is EPIC";
+            Video.appendChild(Vid);
+            InfoHeader.appendChild(Title);
+            InfoHeader.appendChild(Date);
+            Video.appendChild(InfoHeader);
+            Video.appendChild(Description);
+            document.getElementById("Content").appendChild(Video);
         }
     }
 }
